6b12b0c21b98a1c64ef15978cfeccf9bb5943364
Trieu Ho <trieu.ho@flextrade.com>
2021-04-29 14:21:42 +0100

    adding parsing of csv file into a list of strings

--- YTMCalculator.cs
+++ YTMCalculator.cs
@@ -1,53 +1,52 @@
-﻿using System;
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
 
 namespace Calculator
 {
-	class Program
+	public static class YTM
 	{
-		static void Main(string[] args)
-		{
-			// Declare variables and then initialize to zero.
-			int num1 = 0; int num2 = 0;
+        public static List<string> ReadCSV(string filePath)
+        {
+            List<string> listA = new List<string>();
+            StreamReader reader = null;
+            if (File.Exists(filePath))
+            {
+                reader = new StreamReader(File.OpenRead(filePath));
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    foreach (var item in values)
+                    {
+                        if(!string.IsNullOrEmpty(item))
+                        {
+                            listA.Add(item);
+                        }
 
-			// Display title as the C# console calculator app.
-			Console.WriteLine("Console Calculator in C#\r");
-			Console.WriteLine("------------------------\n");
-
-			// Ask the user to type the first number.
-			Console.WriteLine("Type a number, and then press Enter");
-			num1 = Convert.ToInt32(Console.ReadLine());
-
-			// Ask the user to type the second number.
-			Console.WriteLine("Type another number, and then press Enter");
-			num2 = Convert.ToInt32(Console.ReadLine());
-
-			// Ask the user to choose an option.
-			Console.WriteLine("Choose an option from the following list:");
-			Console.WriteLine("\ta - Add");
-			Console.WriteLine("\ts - Subtract");
-			Console.WriteLine("\tm - Multiply");
-			Console.WriteLine("\td - Divide");
-			Console.Write("Your option? ");
-
-			// Use a switch statement to do the math.
-			switch (Console.ReadLine())
-			{
-				case "a":
-					Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
-					break;
-				case "s":
-					Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
-					break;
-				case "m":
-					Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
-					break;
-				case "d":
-					Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
-					break;
-			}
-			// Wait for the user to respond before closing.
-			Console.Write("Press any key to close the Calculator console app...");
-			Console.ReadKey();
-		}
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("File doesn't exist");
+            }
+            return listA;
+        }
 	}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> cashFlowData;
+            cashFlowData = YTM.ReadCSV("FixedIncomeCashflows.csv");
+
+            foreach (var coloumn1 in cashFlowData)
+            {
+                Console.WriteLine(coloumn1);
+            }
+            Console.ReadLine();
+        }
+    }
 }
---
+++ build.bat
@@ -0,0 +1,3 @@
+C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe YTMCalculator.cs
+
+pause
